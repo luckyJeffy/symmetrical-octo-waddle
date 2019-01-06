f17b00cef6fc3c7635a9963930c0a95016a77e85
shenjite <shenjite@DESKTOP-7IG07AP>
2019-01-06 10:47:07 +0800

    开发与发布版本都增加console路径

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/console/',
     // proxyTable: {},
     proxyTable: {
       '/api': {
@@ -71,7 +71,7 @@ module.exports = {
      * then assetsPublicPath should be set to "/bar/".
      * In most cases please use '/' !!!
      */
-    assetsPublicPath: '/',
+    assetsPublicPath: '/console/',
 
     /**
      * Source Maps
