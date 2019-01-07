b8c22a7064e76b61abab58c5f6ed60754e98562b
Allen <446522951@qq.com>
2019-01-07 15:51:31 +0800

    修正开发环境config

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/console/',
+    assetsPublicPath: '/',
     // proxyTable: {},
     proxyTable: {
       '/api': {
